7c950027da5ede2e20eb3c44672df7a355679ef2
Ken <kendo302003@gmail.com>
2023-01-02 18:44:39 +0100

    main text

--- Infinity-Landing/src/section1/main.jsx
+++ Infinity-Landing/src/section1/main.jsx
@@ -4,6 +4,10 @@ const Main = () => {
   return (
     <div>
       <img src={ Backdrop } id="backdrop"/>
+      <h2>More than your regular email inbox</h2>
+      <p>We've helped over 25,000 individuals and companies to grow their business successfully.</p>
+      <a href="#"><button>Start 14 Days Free Trial</button></a>
+      <p>No credit card required</p>
     </div>
   );
 }
