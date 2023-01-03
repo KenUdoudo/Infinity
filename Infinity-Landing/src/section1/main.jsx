7ca84f9d55248c3ccb8b16dea1565265ae26b973
Ken <kendo302003@gmail.com>
2023-01-03 07:18:59 +0100

    line break

--- Infinity-Landing/src/section1/main.jsx
+++ Infinity-Landing/src/section1/main.jsx
@@ -4,8 +4,8 @@ const Main = () => {
   return (
     <div className='main'>
       <img src={ Backdrop } id="backdrop"/>
-      <h2>More than your regular email inbox</h2>
-      <p>We've helped over 25,000 individuals and companies to grow their business successfully.</p>
+      <h2>More than your <br /> regular email inbox</h2>
+      <p>We've helped over 25,000 individuals and companies <br /> to grow their business successfully.</p>
       <a href="#"><button>Start 14 Days Free Trial</button></a>
       <p>No credit card required</p>
     </div>
