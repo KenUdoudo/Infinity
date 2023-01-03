cdbc449c7a2a9606e33d749c07a2d5845a409365
Ken <kendo302003@gmail.com>
2023-01-03 07:34:34 +0100

    importing forbes

--- Infinity-Landing/src/section1/b-main.jsx
+++ Infinity-Landing/src/section1/b-main.jsx
@@ -2,6 +2,7 @@ import GeekWire from './assets/logo 1geekwire2.png'
 import Slack from './assets/logo 2slack.png'
 import Envato from './assets/logo 3envato.png'
 import USA from './assets/logo 4usa.png'
+import Forbes from './assets/logo 5forbes.png'
 
 const Bmain = () => {
   return (
