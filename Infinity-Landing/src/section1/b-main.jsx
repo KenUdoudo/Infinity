a4278a494e4999955b5ce0ccc2088eecd8a69b98
Ken <kendo302003@gmail.com>
2023-01-03 07:33:34 +0100

    importing usa today

--- Infinity-Landing/src/section1/b-main.jsx
+++ Infinity-Landing/src/section1/b-main.jsx
@@ -1,6 +1,7 @@
 import GeekWire from './assets/logo 1geekwire2.png'
 import Slack from './assets/logo 2slack.png'
 import Envato from './assets/logo 3envato.png'
+import USA from './assets/logo 4usa.png'
 
 const Bmain = () => {
   return (
