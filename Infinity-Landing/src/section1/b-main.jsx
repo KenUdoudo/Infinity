6b5f9aab9bdfd90591824b1bb0d49f16afeb6426
Ken <kendo302003@gmail.com>
2023-01-03 18:11:22 +0100

    font and adding img

--- Infinity-Landing/src/section1/b-main.jsx
+++ Infinity-Landing/src/section1/b-main.jsx
@@ -6,7 +6,13 @@ import Forbes from './assets/logo 5forbes.png'
 
 const Bmain = () => {
   return (
-    
+    <div className="bline">
+      <img src={ Geekwire }/>
+      <img src={ Slack }/>
+      <img src={ Envato }/>
+      <img src={ USA }/>
+      <img src={ Forbes }/>
+    </div>
   );
 }
  
