92abfc16f0c68f36f415845ce6055b55dd3aea94
Ken <kendo302003@gmail.com>
2023-01-03 18:33:27 +0100

    changing btn to link

--- Infinity-Landing/src/section1/main.jsx
+++ Infinity-Landing/src/section1/main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
       <img src={ Backdrop } id="backdrop"/>
       <h2>More than your <br /> regular email inbox</h2>
       <p id='p1'>We've helped over 25,000 individuals and companies <br /> to grow their business successfully.</p>
-      <a href="#"  id='mainbtn'><button>Start 14 Days Free Trial</button></a>
+      <a href="#"  id='mainbtn'>Start 14 Days Free Trial</a>
       <p id='p2'>No credit card required</p>
     </div>
   );
