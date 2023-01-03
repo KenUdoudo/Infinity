07d39cfd42140effce7511dc559f48edafaf585e
Ken <kendo302003@gmail.com>
2023-01-03 07:32:27 +0100

    importing envato

--- Infinity-Landing/src/section1/b-main.jsx
+++ Infinity-Landing/src/section1/b-main.jsx
@@ -1,5 +1,6 @@
 import GeekWire from './assets/logo 1geekwire2.png'
 import Slack from './assets/logo 2slack.png'
+import Envato from './assets/logo 3envato.png'
 
 const Bmain = () => {
   return (
