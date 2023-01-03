7eeea5d0e227ab74235fe960f72cd25587825409
Ken <kendo302003@gmail.com>
2023-01-03 07:22:14 +0100

    main btn css

--- Infinity-Landing/src/section1/main.jsx
+++ Infinity-Landing/src/section1/main.jsx
@@ -5,9 +5,9 @@ const Main = () => {
     <div className='main'>
       <img src={ Backdrop } id="backdrop"/>
       <h2>More than your <br /> regular email inbox</h2>
-      <p>We've helped over 25,000 individuals and companies <br /> to grow their business successfully.</p>
-      <a href="#"><button>Start 14 Days Free Trial</button></a>
-      <p>No credit card required</p>
+      <p id='p1'>We've helped over 25,000 individuals and companies <br /> to grow their business successfully.</p>
+      <a href="#"><button id='mainbtn'>Start 14 Days Free Trial</button></a>
+      <p id='p2'>No credit card required</p>
     </div>
   );
 }
