b030518d0e2ea93a05bbfcea669c66f8e9dcee7b
Ken <kendo302003@gmail.com>
2023-01-04 05:18:52 +0100

    import remaining images for section 2

--- Infinity-Landing/src/section2.jsx
+++ Infinity-Landing/src/section2.jsx
@@ -1,5 +1,7 @@
 import './section2/section2.css'
 import Collab from './section2/assets/Iconteam collab.png'
+import Chat from './section2/assets/Iconinstant chat.png'
+import manager from './section2/assets/Iconmedia manager.png'
 const Section2 = () => {
   return (
 
