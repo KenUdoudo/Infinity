c1d22b71245c506f2767ac6c837b5af5098036fe
Ken <kendo302003@gmail.com>
2023-01-04 08:58:56 +0100

    section 2 header

--- Infinity-Landing/src/section2.jsx
+++ Infinity-Landing/src/section2.jsx
@@ -4,7 +4,12 @@ import Chat from './section2/assets/Iconinstant chat.png'
 import manager from './section2/assets/Iconmedia manager.png'
 const Section2 = () => {
   return (
-
+    <div className="sec2">
+      <div className="header">
+        <h2>Solution made for you</h2>
+        <p>These companies release their own versions of the operating systems with minor changes, and yet always.</p>
+      </div>
+    </div>
   );
 }
  
