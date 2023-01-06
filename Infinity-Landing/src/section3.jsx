6ddeb4ce7267f8b85614ff81ce76ff9dcca34656
Ken <kendo302003@gmail.com>
2023-01-06 09:43:55 +0100

    importining section 3 assets

--- Infinity-Landing/src/section3.jsx
+++ Infinity-Landing/src/section3.jsx
@@ -1,3 +1,9 @@
+import './section3/section3.css'
+import connect from './section3/Assets/Icon.png'
+import increase from './section3/Assets/Icon (1).png'
+import unlimited from './section3/Assets/Icon (2).png'
+import sideimg from './section3/Assets/Group 15.png'
+
 
 const Section3 = () => {
   return (
