ff5db8c1b2d4e7f361df7443230d5dfbc39cd337
Ken <kendo302003@gmail.com>
2023-01-07 08:12:33 +0100

    sec3 text

--- Infinity-Landing/src/section3.jsx
+++ Infinity-Landing/src/section3.jsx
@@ -7,7 +7,15 @@ import sideimg from './section3/Assets/Group 15.png'
 
 const Section3 = () => {
   return (
+    <div className="sec3">
+      <div className="main">
+        <h2>Get all your emails in a single place</h2>
+        <p>These companies release their own versions of the operating systems with minor changes, and yet always with the same bottom line.</p>
+      </div>
+      <div className="sideimg">
 
+      </div>
+    </div>
   );
 }
  
