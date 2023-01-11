ac0501ad18d2ffb51d8917949c6f5340b018e1f0
Ken <kendo302003@gmail.com>
2023-01-11 18:19:13 +0100

    making btn clickable

--- Infinity-Landing/src/section1/navbar.jsx
+++ Infinity-Landing/src/section1/navbar.jsx
@@ -1,18 +1,22 @@
 import Hero from './assets/Logo (Case Conflict).png'
 import Hamburger from './assets/Group 1hamburger.png'
+import { useState } from "react";
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navbar">
-      <img src={ Hero }/>
+      <img src={ Hero } />
       <div className="navlinks">
-        <ul className='menu active'>
+        <ul className={`navul ${open? 'active' : 'inactive'}`}>
           <li><a href="#">Demos</a></li>
           <li><a href="#">Pages</a></li>
           <li><a href="#">Support</a></li>
           <a href="#" id='blue-link'>Start 14 Days Free Trial</a>
         </ul>
       </div>
-      <img src={ Hamburger } id='ham'/>
+      <img src={ Hamburger } onClick={()=>{setOpen(!open)}} id='ham'/>
     </div>
   );
 }
