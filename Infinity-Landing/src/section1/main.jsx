7ca2f4b829018c30e33a6483d7d55d8c47615bd5
Ken <kendo302003@gmail.com>
2023-01-13 18:07:45 +0100

    p1 padding

--- Infinity-Landing/src/section1/main.jsx
+++ Infinity-Landing/src/section1/main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
     <div className='main'>
       <img src={ Backdrop } id="backdrop"/>
       <h2>More than your <br /> regular email inbox</h2>
-      <p id='p1'>We've helped over 25,000 individuals and companies <br /> to grow their business successfully.</p>
+      <p id='p1'>We've helped over 25,000 individuals and companies to grow their business successfully.</p>
       <a href="#"  id='mainbtn'>Start 14 Days Free Trial</a>
       <p id='p2'>No credit card required</p>
     </div>
