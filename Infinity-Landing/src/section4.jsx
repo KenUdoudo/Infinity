9004ad0f5aca2c3e090dcf23e244a6f7a45a3d47
Ken <kendo302003@gmail.com>
2023-01-14 19:23:32 +0100

    adding store btn

--- Infinity-Landing/src/section4.jsx
+++ Infinity-Landing/src/section4.jsx
@@ -10,6 +10,9 @@ const Section4 = () => {
         <img src={ sideimg } id='leftimg'/>
         <div className="sec4text">
           <h2>Work anywhere, with any device</h2>
+          <p>These companies release their own versions of the operating systems with minor changes, and yet always with the same bottom line.</p>
+          <img src={ AppStore } id='appstore'/>
+          <img src={ PlayStore } id='playstore'/>
         </div>
       </div>
       </div>
