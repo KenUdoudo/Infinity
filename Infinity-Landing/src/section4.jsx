89d357c7c99269ce840ec6b96af5979641e1924f
Ken <kendo302003@gmail.com>
2023-01-14 19:15:24 +0100

    main section 4 css

--- Infinity-Landing/src/section4.jsx
+++ Infinity-Landing/src/section4.jsx
@@ -5,11 +5,13 @@ import PlayStore from './section4/assets/BTN 2.png'
 
 const Section4 = () => {
   return (
+    <div className="Sec4">
       <div className="sec4">
         <img src={ sideimg } id='leftimg'/>
         <div className="sec4text">
           <h2>Work anywhere, with any device</h2>
         </div>
+      </div>
       </div>
   );
 }
