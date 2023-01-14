ef55fb33f2f881fb6703f52b64438a2edd9a577a
Ken <kendo302003@gmail.com>
2023-01-14 19:03:59 +0100

    importing section4

--- Infinity-Landing/src/App.jsx
+++ Infinity-Landing/src/App.jsx
@@ -1,6 +1,7 @@
 import Section1 from "./section1"
 import Section2 from "./section2"
 import Section3 from "./section3"
+import Section4 from "./section4"
 function App() {
   
 
@@ -11,6 +12,8 @@ function App() {
       <Section2 />
 
       <Section3 />
+
+      <Section4 />
     </div>
   )
 }
