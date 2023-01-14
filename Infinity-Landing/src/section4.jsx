654b051729bbb4d6422872eddc5127b1485a98a0
Ken <kendo302003@gmail.com>
2023-01-14 18:39:31 +0100

    importing section4 img

--- Infinity-Landing/src/section4.jsx
+++ Infinity-Landing/src/section4.jsx
@@ -1,8 +1,11 @@
 import './section4/section4.css'
+import sideimg from './section4/assets/Group 16.png'
+import AppStore from './section4/assets/BTN 1.png'
+import PlayStore from './section4/assets/BTN 2.png'
 
 const Section4 = () => {
   return (
-
+      
   );
 }
  
