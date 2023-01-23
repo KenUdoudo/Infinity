30ffb2071348edd78e361bd06eb8b8f898e26fa4
Ken <kendo302003@gmail.com>
2023-01-23 18:08:03 +0100

    other plans

--- Infinity-Landing/src/section6.jsx
+++ Infinity-Landing/src/section6.jsx
@@ -22,6 +22,34 @@ const Section6 = () => {
         </div>
         <a href="#" id='white-link'>Start 14 Days Free Trial</a>
       </div>
+      <img src={ line } id="line"/>
+      <div className="sections">
+        <h3>Standard</h3>
+        <div className="storage">
+          <p>100 GB Storage</p>
+          <p>50 Accounts</p>
+          <p>5,000 Connections</p>
+        </div>
+        <div className="price">
+          <h2>$468</h2>
+          <p>$39 per month, billed yearly</p>
+        </div>
+        <a href="#" id='white-link'>Start 14 Days Free Trial</a>
+      </div>
+      <img src={ line } id="line"/>
+      <div className="sections">
+        <h3>Pro</h3>
+        <div className="storage">
+          <p>Unlimited GB Storage</p>
+          <p>Unlimited Accounts</p>
+          <p>Unlimited Connections</p>
+        </div>
+        <div className="price">
+          <h2>$708</h2>
+          <p>$59 per month, billed yearly</p>
+        </div>
+        <a href="#" id='blue-link2'>Start 14 Days Free Trial</a>
+      </div>
     </div>
   );
 }
