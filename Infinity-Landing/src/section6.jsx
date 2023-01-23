b38e0b6e7e57338ea1f254f2e3e6bb2ece43eb62
Ken <kendo302003@gmail.com>
2023-01-23 17:36:52 +0100

    starter section

--- Infinity-Landing/src/section6.jsx
+++ Infinity-Landing/src/section6.jsx
@@ -8,6 +8,19 @@ const Section6 = () => {
         <h2>Choose the right plan for your finity</h2>
         <img src={ switchbtn }/>
       </div>
+      <div className="starter">
+        <h3>Starter</h3>
+        <div className="storage">
+          <p>10 GB Storage</p>
+          <p>15 Accounts</p>
+          <p>1,000 Connections</p>
+        </div>
+        <div className="price">
+          <h2>$228</h2>
+          <p>$19 per month, billed yearly</p>
+        </div>
+        <a href="#" id='white-link'>Start 14 Days Free Trial</a>
+      </div>
     </div>
   );
 }
