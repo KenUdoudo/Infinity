cbd250dbdec72adf84fc80ca63f7efaba4a9f71b
Ken <kendo302003@gmail.com>
2023-01-23 17:04:32 +0100

    sec6head

--- Infinity-Landing/src/section6.jsx
+++ Infinity-Landing/src/section6.jsx
@@ -1,8 +1,14 @@
 import './section6/section6.css'
+import switchbtn from './section6/assets/Switch.png'
 
 const Section6 = () => {
   return (
-
+    <div className="sec6">
+      <div className="sec6head">
+        <h2>Choose the right plan for you</h2>
+        <img src={ switchbtn }/>
+      </div>
+    </div>
   );
 }
  
