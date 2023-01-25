9e2d46cd60b8229afc656ebec0345b2f7dca1f42
Ken <kendo302003@gmail.com>
2023-01-25 18:18:27 +0100

    importing footerlogo

--- Infinity-Landing/src/section7.jsx
+++ Infinity-Landing/src/section7.jsx
@@ -1,8 +1,12 @@
 import './section7/section7.css'
+import footerLogo from './section7/assets/Logo white.png'
+
 
 const Section7 = () => {
   return (
-
+    <div className="sec7">
+      <div className="part1"><img src={ footerLogo } /></div>
+    </div>
   );
 }
  
