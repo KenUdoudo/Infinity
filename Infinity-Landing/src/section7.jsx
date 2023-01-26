f229b632f9e64e445f858b93f03bf3b040945675
Ken <kendo302003@gmail.com>
2023-01-26 16:48:46 +0100

    footerbg css

--- Infinity-Landing/src/section7.jsx
+++ Infinity-Landing/src/section7.jsx
@@ -4,8 +4,8 @@ import footerbg from './section7/assets/Group 17.png'
 
 const Section7 = () => {
   return (
-    
     <div className="sec7">
+      <img src={ footerbg } id="footerbg"/>
       <div className="part1"><img src={ footerLogo } /></div>
       <div className="part2"></div>
       <div className="part3"></div>
