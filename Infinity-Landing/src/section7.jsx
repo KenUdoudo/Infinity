29ed08e5b74c162eb8216fca831098add492e49f
Ken <kendo302003@gmail.com>
2023-01-27 16:20:32 +0100

    converting p to links

--- Infinity-Landing/src/section7.jsx
+++ Infinity-Landing/src/section7.jsx
@@ -14,21 +14,21 @@ const Section7 = () => {
       </div>
       <div className="part2">
         <p id="parthead">Community</p>
-        <p id="white">For Talents</p>
-        <p id="white">For Companies</p>
-        <p id="white">Facebook Group</p>
-        <p id="white">FAQ</p>
+        <a href="#"><p id="white">For Talents</p></a>
+        <a href="#"><p id="white">For Companies</p></a>
+        <a href="#"><p id="white">Facebook Group</p></a>
+        <a href="#"><p id="white">FAQ</p></a>
       </div>
       <div className="part3">
       <p id="parthead">About us</p>
-        <p id="white">Meet the Team</p>
-        <p id="white">Our Story</p>
-        <p id="white">Career</p>
+      <a href="#"><p id="white">Meet the Team</p></a>
+      <a href="#"><p id="white">Our Story</p></a>
+      <a href="#"><p id="white">Career</p></a>
       </div>
       <div className="part4">
       <p id="parthead">Contacts</p>
-        <p id="white" className='part4text'>Feel free to get in touch with us via phone or send us a message.</p>
-        <p id="gold">+1-301-340-3946 <br />info@infinity.com</p>
+      <p id="white" className='part4text'>Feel free to get in touch with us via phone or send us a message.</p>
+      <a href="#"><p id="gold">+1-301-340-3946 <br />info@infinity.com</p></a>
         
       </div>
       </div>
